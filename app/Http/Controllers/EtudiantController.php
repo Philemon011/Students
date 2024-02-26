496010592e540408e69b02d76a91df5b4bc5e471
Philemon <etoundelight1@gmail.com>
2024-02-26 16:33:29 +0100

    Ajout de la page d'accueil

---
+++ app/Http/Controllers/EtudiantController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EtudiantController extends Controller
+{
+   public function liste_etudiant(){
+        return view('etudiant.liste');
+   }
+
+}
